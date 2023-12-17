2bace36e727736b2d899d0c2e1f57a611548f549
Ajay K <76083469+ajaypep@users.noreply.github.com>
2023-12-18 00:17:18 +0530

    feat: Add beginGameRound

--- app.js
+++ app.js
@@ -3,6 +3,49 @@ function getRandomInt(upperBoundExclusive) {
 }
 
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     return choices[getRandomInt(choices.length)];
+}
+
+function beginGameRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (playerChoice === computerChoice) {
+        return 'I\'s a TIE';
+    }
+    let result;
+    switch (playerChoice) {
+        case 'rock':
+            {
+                // computer's choice can either be scissors or paper
+                if (computerChoice === 'scissors') {
+                    result = 'You won, rock beats scissors';
+                } else {
+                    result = 'You lost, paper beats rock';
+                }
+                break;
+            }
+        case 'paper':
+            {
+                // computer's choice can either be rock or scissors
+                if (computerChoice === 'rock') {
+                    result = 'You won, paper beats rock';
+                } else {
+                    result = 'You lost, scissors beats paper';
+                }
+                break;
+            }
+        case 'scissors':
+            {
+                // computer's choice can either be paper or rock
+                if (computerChoice === 'paper') {
+                    result = 'You won, scissors beats paper';
+                } else {
+                    result = 'You lost, rock beats scissors';
+                }
+                break;
+            }
+
+    }
+    return result;
 }
